e9fc7ea38b3d3241b33934fd39545b08582a9ee9
pananananas <101266606+pananananas@users.noreply.github.com>
2023-08-24 14:20:25 +0200

    added bicubic inteerpolation and download button

--- upscaler-website/src/components/HelloWorld.vue
+++ upscaler-website/src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <select v-model="selectedAlgorithm">
       <option value="nearestNeighbor">Nearest Neighbor</option>
       <option value="bilinear">Bilinear Interpolation</option>
+			<option value="bicubic">Bicubic Interpolation</option>
     </select>
     <div ref="dropArea" 
          @click="selectFile" 
@@ -10,10 +11,13 @@
          @dragover.prevent 
          @dragenter.prevent 
          class="drop-area">
-      Przeciągnij obraz tutaj lub kliknij, aby wybrać
+      Przeciągnij obraz tutaj lub kliknij, aby wybrać powiększany obraz.
     </div>
     <input type="file" ref="fileInput" @change="handleFileChange" style="display: none" />
+		<button v-if="newWidth && newHeight" @click="downloadImage">Pobierz</button>
+		<div>
     <canvas v-if="newWidth && newHeight" ref="canvas" :width="newWidth" :height="newHeight"></canvas>
+		</div>
   </div>
 </template>
 
@@ -25,27 +29,44 @@ export default {
       selectedAlgorithm: "nearestNeighbor",
       newWidth: null, 
       newHeight: null,
+			originalFilename: null,
     };
   },
   methods: {
-    async handleDrop(event) {
-      event.preventDefault();
-      const file = event.dataTransfer.files[0];
-      this.processImage(file);
+    downloadImage() {
+      const canvas = this.$refs.canvas;
+      const dataURL = canvas.toDataURL("image/png");
+      const downloadLink = document.createElement('a');
+      downloadLink.href = dataURL;
+      downloadLink.download = this.getDownloadFilename();
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
     },
-    selectFile() {
-      this.$refs.fileInput.click();
+    getDownloadFilename() {
+      const baseName = this.originalFilename ? this.originalFilename.split('.')[0] : "image";
+      return `${baseName}_${this.selectedAlgorithm}.png`;
     },
     handleFileChange(event) {
       const file = event.target.files[0];
+      this.originalFilename = file.name;
       this.processImage(file);
+    },
+    handleDrop(event) {
+      event.preventDefault();
+      const file = event.dataTransfer.files[0];
+      this.originalFilename = file.name;
+      this.processImage(file);
+	},	
+    selectFile() {
+      this.$refs.fileInput.click();
     },
     async processImage(file) {
       if (file && file.type.startsWith("image/")) {
         const image = await this.createImageFromFile(file);
         
-        this.newWidth = image.width * 2;
-        this.newHeight = image.height * 2;
+        this.newWidth = image.width * 4;
+        this.newHeight = image.height * 4;
 
         this.$nextTick(() => {
           const canvas = this.$refs.canvas;
@@ -59,7 +80,9 @@ export default {
             this.bilinearInterpolation(srcData, dstData, image.width, image.height);
           } else if (this.selectedAlgorithm === "nearestNeighbor") {
             this.nearestNeighbor(srcData, dstData, image.width, image.height);
-          }
+          } else if (this.selectedAlgorithm === "bicubic") {
+						this.bicubicInterpolation(srcData, dstData, image.width, image.height);
+					}
 
           ctx.putImageData(dstData, 0, 0);
         });
@@ -121,6 +144,100 @@ export default {
 				}
 			}
 		},
+
+    // ... pozostałe metody ...
+
+    bicubicInterpolation(srcData, dstData, srcWidth, srcHeight) {
+        // const srcPixels = srcData.data;
+        const dstPixels = dstData.data;
+
+        const ratioX = srcWidth / dstData.width;
+        const ratioY = srcHeight / dstData.height;
+
+        const cubicInterpolate = (p, x) => {
+            return p[1] + 0.5 * x * (p[2] - p[0] + x * (2.0 * p[0] - 5.0 * p[1] + 4.0 * p[2] - p[3] + x * (3.0 * (p[1] - p[2]) + p[3] - p[0])));
+        };
+
+        const getPixel = (imageData, x, y) => {
+            const position = (x + y * imageData.width) * 4;
+            return [
+                imageData.data[position],
+                imageData.data[position + 1],
+                imageData.data[position + 2],
+                imageData.data[position + 3]
+            ];
+        };
+
+        for (let y = 0; y < dstData.height; y++) {
+            for (let x = 0; x < dstData.width; x++) {
+                const gx = x * ratioX;
+                const gy = y * ratioY;
+
+                const gxi = Math.floor(gx);
+                const gyi = Math.floor(gy);
+
+                const c = [];
+                for (let j = -1; j <= 2; j++) {
+                    const rowData = [];
+                    for (let i = -1; i <= 2; i++) {
+                        rowData.push(getPixel(srcData, gxi + i, gyi + j));
+                    }
+                    c.push(rowData);
+                }
+
+                const red = [];
+                const green = [];
+                const blue = [];
+                const alpha = [];
+                for (let j = 0; j < 4; j++) {
+                    const r = cubicInterpolate([
+                        c[j][0][0],
+                        c[j][1][0],
+                        c[j][2][0],
+                        c[j][3][0]
+                    ], gx - gxi);
+
+                    const g = cubicInterpolate([
+                        c[j][0][1],
+                        c[j][1][1],
+                        c[j][2][1],
+                        c[j][3][1]
+                    ], gx - gxi);
+
+                    const b = cubicInterpolate([
+                        c[j][0][2],
+                        c[j][1][2],
+                        c[j][2][2],
+                        c[j][3][2]
+                    ], gx - gxi);
+
+                    const a = cubicInterpolate([
+                        c[j][0][3],
+                        c[j][1][3],
+                        c[j][2][3],
+                        c[j][3][3]
+                    ], gx - gxi);
+
+                    red.push(r);
+                    green.push(g);
+                    blue.push(b);
+                    alpha.push(a);
+                }
+
+                const dstIndex = (y * dstData.width + x) * 4;
+
+                dstPixels[dstIndex] = cubicInterpolate(red, gy - gyi);
+                dstPixels[dstIndex + 1] = cubicInterpolate(green, gy - gyi);
+                dstPixels[dstIndex + 2] = cubicInterpolate(blue, gy - gyi);
+                dstPixels[dstIndex + 3] = cubicInterpolate(alpha, gy - gyi);
+            }
+        }
+    },
+
+
+
+
+
     async createImageFromFile(file) {
       return new Promise((resolve, reject) => {
         const image = new Image();
